aa24cef2591bc62be5360c4e772e764bcd36dff4
elvinsomon <elvinsomon2001@gmail.com>
2020-07-17 23:44:23 -0400

    Codigo comentado.

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+
+    <!-- Tailwind CSS "PARA LOS ESTILOS CSS" -->
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    
+    <!-- FUENTES -->
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet"> 
     <title>Calculadora Pro</title>
 </head>
@@ -14,10 +17,12 @@
         <div class="container mx-auto my-auto  w-1/3 py-8 mt-6 text-center bg-blue-800 border-4 border-gray-400 rounded shadow-2xl">
             <h1 class="text-4xl font-bold text-gray-300 text-center">Calculadora Pro</h1>
             
+            <!-- FORMULARIO -->
             <form method="get" action="" class="mt-5">
                 <input class="placeholder-gray-500 border rounded p-1" type="text" name="numero1" id="" placeholder="Ingrese un Valor"> 
                 <br>
 
+                <!-- COMBO BOX PARA SELECCIONAR LA OPERACION -->
                 <select class=" mx-auto mt-4 mb-4 bg-gray-200 border border-gray-200 text-gray-800 py-2 px-2 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="opciones" id="">
                     <option value="0">+</option>
                     <option value="1">-</option>
@@ -28,11 +33,17 @@
 
                 <input class="placeholder-gray-500 border rounded p-1" type="text" name="numero2" id="" placeholder="Ingrese un Valor"> 
                 <br>
+
+                <!-- BOTON -->
                 <input class="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-3" type="submit" name="calcular" value="Calcular">
 
-                <output type='number' name="">
+                <!-- CODIGO PHP -->
+                <!-- El codigo php se coloco en esta zona por conveniencia del diseño de la pagina. 
+                     Si colobamos el codigo php en el lugar convencional se veria sacrificado el flujo del DOM. 
+                -->
                 <?php
 
+                // SE APLICA EL USO DE FUNCIONES PARA DEVOLVER EL RESULTADO DE LA OPERACION
                 function sumar($num1, $num2)
                 {
                     $result = $num1 + $num2;
@@ -54,11 +65,13 @@
                     return '<p class="bg-blue-500  mx-auto w-1/2 mt-3 rounded">Su resultado es: '.$result.'</p>';
                 }
 
+                // SE RECIBE LA PETICION DEL FORMULARIO
                 if (isset($_REQUEST['calcular'])) {
                     $num1 = $_REQUEST['numero1'];
                     $num2 = $_REQUEST['numero2'];
                     $op = $_REQUEST['opciones'];
                     
+                    // SE LLAMA A LA FUNCION DESEADA
                     switch ($op) {
                         case 0:
                             echo sumar($num1, $num2);
